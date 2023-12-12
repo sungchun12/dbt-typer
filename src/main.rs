30b25c4750ad3bd8f46ada5ffe30ca2d7b0d332a
Sung Won Chung <sung@datafold.com>
2023-12-12 15:21:42 -0800

    quick file read and append example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+use std::path::Path;
+
+fn append_line_to_schema(path: &str, line: &str) -> std::io::Result<()> {
+    let path = Path::new(path).join("schema.yml");
+
+    let mut file = OpenOptions::new()
+                    .write(true)
+                    .create(true)
+                    .append(true)
+                    .open(&path)?;
+
+    writeln!(file, "{}", line)?;
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let path = "/Users/sung/Desktop/git_repos/dbt-typer/tests/test_dbt_project/models";
+    let line = "hello world";
+
+    match append_line_to_schema(path, line) {
+        Err(e) => eprintln!("Error writing to file: {}", e),
+        _ => (),
+    }
 }
