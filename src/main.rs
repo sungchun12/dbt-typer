f95e2300e70b4a06a934f2f2ea10dddc377f86ec
sungchun12 <sungwonchung3@gmail.com>
2023-12-14 21:58:25 -0800

    Add clap dependency and update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,60 @@
-use std::fs::OpenOptions;
-use std::io::prelude::*;
-use std::path::Path;
+use std::path::PathBuf;
 
-fn append_line_to_schema(path: &str, line: &str) -> std::io::Result<()> {
-    let path = Path::new(path).join("schema.yml");
-
-    let mut file = OpenOptions::new()
-                    .write(true)
-                    .create(true)
-                    .append(true)
-                    .open(&path)?;
-
-    writeln!(file, "{}", line)?;
-
-    Ok(())
-}
+use clap::{arg, command, value_parser, ArgAction, Command};
 
 fn main() {
-    let path = "/Users/sung/Desktop/git_repos/dbt-typer/tests/test_dbt_project/models";
-    let line = "hello world";
+    let matches = command!() // requires `cargo` feature
+        .arg(arg!([name] "Optional name to operate on")) // positional argument does NOT require flag
+        .arg(
+            arg!(
+                -c --config <FILE> "Sets a custom config file"
+            )
+            // We don't have syntax yet for optional options, so manually calling `required`
+            .required(false)
+            .value_parser(value_parser!(PathBuf)),
+        )
+        .arg(arg!(
+            -d --debug ... "Turn debugging information on"
+        ))
+        .subcommand(
+            Command::new("test")
+                .about("does testing things")
+                .arg(arg!(-l --list "lists test values").action(ArgAction::SetTrue)),
+        )
+        .get_matches();
 
-    match append_line_to_schema(path, line) {
-        Err(e) => eprintln!("Error writing to file: {}", e),
-        _ => (),
+    // You can check the value provided by positional arguments, or option arguments
+    if let Some(name) = matches.get_one::<String>("name") {
+        println!("Value for name: {name}");
     }
+
+    if let Some(config_path) = matches.get_one::<PathBuf>("config") {
+        println!("Value for config: {}", config_path.display());
+    }
+
+    // You can see how many times a particular flag or argument occurred
+    // Note, only flags can have multiple occurrences
+    match matches
+        .get_one::<u8>("debug")
+        .expect("Count's are defaulted")
+    {
+        0 => println!("Debug mode is off"),
+        1 => println!("Debug mode is kind of on"),
+        2 => println!("Debug mode is on"),
+        _ => println!("Don't be crazy"),
+    }
+
+    // You can check for the existence of subcommands, and if found use their
+    // matches just as you would the top level cmd
+    if let Some(matches) = matches.subcommand_matches("test") {
+        // "$ myapp test" was run
+        if matches.get_flag("list") {
+            // "$ myapp test -l" was run
+            println!("Printing testing lists...");
+        } else {
+            println!("Not printing testing lists...");
+        }
+    }
+
+    // Continued program logic goes here...
 }
